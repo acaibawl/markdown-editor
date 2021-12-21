4695a7d3a8e0990e24068dfddecd1c31d33f483e
acaiibawl <hagiya0719@gmail.com>
2021-12-21 11:03:33 +0900

    editorに入力した文字をstate管理

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -45,8 +45,10 @@ const Preview = styled.div`
   width: 50vw;
 `;
 
+const StorageKey = 'pages/editor:text';
+
 export const Editor: React.FC = () => {
-  const [text, setText] = useState<string>('');
+  const [text, setText] = useState<string>(localStorage.getItem(StorageKey) || '');;
 
   return (
     <>
@@ -56,6 +58,8 @@ export const Editor: React.FC = () => {
       <Wrapper>
         <TextArea
           onChange={(event) => {
+            const changedText = event.target.value;
+            localStorage.setItem(StorageKey, changedText);
             setText(event.target.value);
           }}
           value={text}
